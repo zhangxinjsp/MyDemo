e1f4a050f9fc6725ee4f4b52f91e83b0ba6e7024
zhangxin <zx366898509@gmail.com>
2014-12-15 10:42:56 +0800

    添加ZBar需要使用的framework注意事项

--- UINavgationController/二维码/ZBarViewController.h
+++ UINavgationController/二维码/ZBarViewController.h
@@ -5,7 +5,14 @@
 //  Created by xsw on 14/12/12.
 //  Copyright (c) 2014年 niexin. All rights reserved.
 //
-
+/*
+ 需要倒入的相关framework
+ libiconv.dylib
+ QuartzCore.framework
+ CoreVideo.framework
+ CoreMedia.framework
+ AVfoundation.framework
+ */
 #import <UIKit/UIKit.h>
 #import "BaseViewController.h"
 #import "ZBarSDK.h"
